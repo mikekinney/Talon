268629b498aa160038ff6f776c23f644be4a1823
Mike Kinney <mike.kinney@mac.com>
2019-01-29 13:35:43 -0600

    Initial Commit

---
+++ Talon/Talon.h
@@ -0,0 +1,19 @@
+//
+//  Talon.h
+//  Talon
+//
+//  Created by Mike on 1/29/19.
+//  Copyright © 2019 Mike Kinney. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Talon.
+FOUNDATION_EXPORT double TalonVersionNumber;
+
+//! Project version string for Talon.
+FOUNDATION_EXPORT const unsigned char TalonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Talon/PublicHeader.h>
+
+
