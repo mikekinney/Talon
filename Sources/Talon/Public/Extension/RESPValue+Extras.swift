611dc6a3c6a63cea7bb101b678aea98cb4080ebb
Mike Kinney <mike.kinney@mac.com>
2020-12-26 08:48:55 -0600

    Add geoJSON extension
    Try mapping a RESPValue to expected array of GeoJSON objects.

---
+++ Sources/Talon/Public/Extension/RESPValue+Extras.swift
@@ -0,0 +1,53 @@
+//
+//  RESPValue+Extras.swift
+//  CNIOAtomics
+//
+//  Created by Mike Kinney on 12/26/20.
+//
+
+import Foundation
+import GEOSwift
+import RediStack
+
+extension RESPValue {
+    public var geoJSON: [GeoJSON] {
+        // FIXME: This should be more generic instead of expecting a specific structure.
+        var json: [GeoJSON] = []
+        switch self {
+        case .array(let array):
+            guard let last = array.last else {
+                return json
+            }
+            switch last {
+            case .array(let array):
+                array.forEach { value in
+                    switch value {
+                    case .array(let array):
+                        guard let last = array.last else {
+                            break
+                        }
+                        switch last {
+                        case .bulkString:
+                            guard let data = last.data else {
+                                break
+                            }
+                            guard let geoJSON = try? JSONDecoder().decode(GeoJSON.self, from: data) else {
+                                break
+                            }
+                            json.append(geoJSON)
+                        default:
+                            break
+                        }
+                    default:
+                        break
+                    }
+                }
+            default:
+                break
+            }
+        default:
+            break
+        }
+        return json
+    }
+}
